933ef6ec5ed46c7d38aad33fed8f58693ad69fae
Dane Wilson <dnkw@hotmail.com>
2022-12-22 19:13:21 -0500

    WIP
    Signed-off-by: Dane Wilson <dnkw@hotmail.com>

--- Sources/FyreKit/ViewControllers/FyreKitViewController.swift
+++ Sources/FyreKit/ViewControllers/FyreKitViewController.swift
@@ -221,11 +221,13 @@ public class FyreKitViewController : UINavigationController {
     
     Log.i("FyreKitViewController: pushToken is \(String(describing: FyreKit.pushToken))")
     
+    Log.i("FyreKitViewController: session is \(self.session)")
+    
     let webView = self.session.webView
-    let script = "register('\(FyreKit.pushToken!)', 'ios');"
+    let script = "window.bridge.register('\(FyreKit.pushToken!)', 'ios')"
     
     Log.i("FyreKitViewController: saying hello")
-    webView.evaluateJavaScript("sayHello()") { object, error in
+    webView.evaluateJavaScript("window.bridge.sayHello()") { object, error in
       if error != nil {
         Log.i("FyreKitViewController: FyreKit saying hello \(String(describing: error))")
       } else if object != nil {
